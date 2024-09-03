1d7838552a4ebc73eb99ce7625b92af7181d2052
Harsh0572 <111284280+Harsh0572@users.noreply.github.com>
2024-09-03 12:23:46 +0530

    Add files via upload

---
+++ oops.py
@@ -0,0 +1,10 @@
+class Atm:
+    def __init__(self):
+        user_input=(input("Enter no:"))
+        if user_input=="1":
+            print("Hello")
+        else:
+            print("No")
+
+atm=Atm() 
+
